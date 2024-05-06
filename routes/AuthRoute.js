80898076d0db0f0a8372b881c9755129860cadfc
Esteban Espinoza <estebandesarrolladordesoftware@gmail.com>
2024-05-06 17:25:59 -0500

    yeah

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { Login, logOut, Me } from "../controllers/Auth.js";
 
-const router = express.Router();
+const router = express.Router()
 
 router.get("/me", Me);
 router.post("/login", Login);
