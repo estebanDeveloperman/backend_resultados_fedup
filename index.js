0021749c8a5ab356b333b23af8afa9c6092225b8
Esteban Espinoza <estebandesarrolladordesoftware@gmail.com>
2024-05-09 11:17:14 -0500

    lets go

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ const store = new sessionStore({
   db: db,
 });
 
-(async () => {
-   await db.sync();
- })();
+// (async () => {
+//    await db.sync();
+//  })();
 
 // codigo para produccion
 app.use(
